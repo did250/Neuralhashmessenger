4b2d6518f64beca84cbbf268c81a1b1d97df2b0c
seyrinn <79247355+seyrinn@users.noreply.github.com>
2022-08-01 15:48:13 +0900

    database read test

--- flutter_messenger_app/lib/src/pages/FriendTab.dart
+++ flutter_messenger_app/lib/src/pages/FriendTab.dart
@@ -6,15 +6,37 @@ class FriendTab extends StatefulWidget {
   _FriendTabState createState() => _FriendTabState();
 }
 
+Future<void> initFriend() async {
+  final DatabaseReference ref = FirebaseDatabase.instance.ref();
+  final snapshot = await ref.child('ChattingRoom').get();
+  if (snapshot.exists) {
+    print(snapshot.value);
+  } else {
+    print('No data available.');
+  }
+}
+
 class _FriendTabState extends State<FriendTab> {
+  //List<Friend> _friends = [];
   @override
-  DatabaseReference ref = FirebaseDatabase.instance.ref();
+  void initState() {
+    //super.initState();
+    initFriend();
+  }
+  //DatabaseReference ref = FirebaseDatabase.instance.ref('');
+
   //FirebaseDatabase database = FirebaseDatabase.instance;
+  /* ex)
+  DatabaseReference starCountRef =
+        FirebaseDatabase.instance.ref('posts/$postId/starCount');
+starCountRef.onValue.listen((DatabaseEvent event) {
+    final data = event.snapshot.value;
+    updateStarCount(data);
+});*/
+
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Center(child: Text("Friend")),
-      ),
+      body: Center(child: Text("Test")),
     );
   }
 }

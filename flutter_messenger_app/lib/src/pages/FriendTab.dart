9de018854c1d782af212c783abe42e82e2356a3d
seyrinn <79247355+seyrinn@users.noreply.github.com>
2022-08-05 17:59:10 +0900

    testing

--- flutter_messenger_app/lib/src/pages/FriendTab.dart
+++ flutter_messenger_app/lib/src/pages/FriendTab.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 
@@ -6,9 +8,19 @@ class FriendTab extends StatefulWidget {
   _FriendTabState createState() => _FriendTabState();
 }
 
-Future<void> initFriend() async {
-  final DatabaseReference ref = FirebaseDatabase.instance.ref();
-  final snapshot = await ref.child('ChattingRoom').get();
+/*
+class ParsedUserData {
+  String name;
+
+}*/
+class Test {
+  final String name;
+  final int id;
+  Test(this.name, this.id);
+}
+
+Future<void> initFriend(DatabaseReference ref) async {
+  final snapshot = await ref.child('User/hihi').get();
   if (snapshot.exists) {
     print(snapshot.value);
   } else {
@@ -16,13 +28,79 @@ Future<void> initFriend() async {
   }
 }
 
+Future<void> readData(DatabaseReference ref) async {
+  final snapshot = await ref.child('User/hihi/Friend').get();
+
+  var friendlist = snapshot.value;
+  //int item;
+  List<Object> friendList = [];
+  // FriendList friends = new FriendList.fromJson(jsonResponse);
+  /*
+  for (item in List<int>.from(snapshot.value as List<Object?>)) {
+    friendList.add(item);
+  }*/
+  //json.decode(snapshot.value.toString());
+  if (snapshot.exists) {
+    print(snapshot.value);
+  } else {
+    print('No data available.');
+  }
+
+  //print(friendList[0]);
+}
+
 class _FriendTabState extends State<FriendTab> {
+  final DatabaseReference ref = FirebaseDatabase.instance.ref();
   //List<Friend> _friends = [];
+
+  /*
   @override
   void initState() {
     //super.initState();
-    initFriend();
+    initFriend(ref);
+  }*/
+
+  Future<void> _addFriend() async {
+    setState(() {
+      DatabaseReference ref = FirebaseDatabase.instance.ref("User/hihi");
+
+      int friendCount = 1;
+      final friendData = {
+        'uid': 123,
+        'name': 'abc',
+      };
+
+      final newPostKey =
+          FirebaseDatabase.instance.ref().child('posts').push().key;
+      final Map<String, Map> updates = {};
+      updates['/User/hihi/Friend/$newPostKey'] = friendData;
+      //updates['/User/hihi/$uid/$newPostKey'] = postData;
+      FirebaseDatabase.instance.ref().update(updates);
+    });
   }
+
+  void _read() {
+    setState(() {
+      final DatabaseReference ref = FirebaseDatabase.instance.ref();
+      readData(ref);
+
+      //test(ref);
+      //snapshot 처리 필요
+      //getfriendnum
+      /*
+      int friendCount = 1;
+      final friendData = {
+        'name': ['a', 'b', 'c'],
+      };
+      final newPostKey =
+          FirebaseDatabase.instance.ref().child('posts').push().key;
+      final Map<String, Map> updates = {};
+      updates['/User/hihi/Friend/$newPostKey'] = friendData;
+      //updates['/User/hihi/$uid/$newPostKey'] = postData;
+      FirebaseDatabase.instance.ref().update(updates);*/
+    });
+  }
+
   //DatabaseReference ref = FirebaseDatabase.instance.ref('');
 
   //FirebaseDatabase database = FirebaseDatabase.instance;
@@ -36,7 +114,14 @@ starCountRef.onValue.listen((DatabaseEvent event) {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Test")),
-    );
+        body: Column(
+      children: [
+        TextButton(child: Text("write"), onPressed: _addFriend),
+        TextButton(
+          child: Text("read"),
+          onPressed: _read,
+        )
+      ],
+    ));
   }
 }

a4e775f60389b3c4adc1906e427a6e2cdc127af3
did250 <gmldnjs2500@naver.com>
2022-08-09 19:58:20 +0900

    Messages

--- flutter_messenger_app/lib/src/pages/ChatRoom.dart
+++ flutter_messenger_app/lib/src/pages/ChatRoom.dart
@@ -1,26 +1,80 @@
+import 'dart:convert';
+
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-const String _name = "MYNAME";
+const String _name = "양희원";
+String _other = "";
+int chattingroom = 1;
 
 
 class ChatRoom extends StatefulWidget {
   final String name;
 
   const ChatRoom(this.name);
-  ChatRoomState createState() => ChatRoomState();
+
+  ChatRoomState createState() => ChatRoomState(this.name);
+
+
 }
 
 class ChatRoomState extends State<ChatRoom> with TickerProviderStateMixin{
-  final List<Messages> _message = <Messages>[];
+  List<Messages> _message = <Messages>[];
+  List<int> _checked = <int>[];
+  String friendname = "";
+
+  ChatRoomState(this.friendname);
   final TextEditingController _textController = TextEditingController();
   bool _exist = false;
+
+  // Future<void> getmember() async {
+  //   final DatabaseReference ref = FirebaseDatabase.instance.ref();
+  //   final snapshot = await ref.child('ChattingRoom').child(chattingroom.toString()).child('Members').get();
+  //   if ( snapshot.exists) {
+  //     for ( var item in List<String>.from(snapshot.value as List<Object?>) ) {
+  //
+  //     }
+  //   }
+  // }
+
+  /// 메시지들 불러와서 저장하는 함수
+  Future<void> readMessages() async {
+    final DatabaseReference ref = FirebaseDatabase.instance.ref();
+    final snapshot = await ref.child('ChattingRoom').child(chattingroom.toString()).child('Messages').get();
+    if ( snapshot.exists ) {
+      for ( var item in (snapshot.value as List<Object?>)){
+        bool mine = false;
+        Map<String,dynamic> map = Map<String, dynamic>.from(item as Map<dynamic?, dynamic?>);
+        if (map['sender'] == _name) {
+          mine = true;
+        }
+        Messages mas = Messages(text: map['text'], animationController: AnimationController(duration : Duration(milliseconds: 0),vsync: this), ismine: mine,);
+        setState(() {
+          _message.insert(0, mas);
+        });
+        mas.animationController.forward();
+        _checked.insert(0, map['checked']);
+      }
+    }
+  }
+  /// 메세지 하나 보낼 때, 서버에 갱신하는 함수
+  Future<void> updatemessage(String input) async {
+    final DatabaseReference ref = FirebaseDatabase.instance.ref();
+    await ref.child('ChattingRoom').child(chattingroom.toString()).child('Messages').child((_message.length).toString()).set({
+      "checked": 1,
+      "sender": _name,
+      "text": input,
+    });
+  }
+
 
 
   @override
   void initState() {
     // TODO: implement initState
-    _aaa();
+    _other = this.friendname;
+    readMessages();
     super.initState();
   }
 
@@ -76,9 +130,8 @@ class ChatRoomState extends State<ChatRoom> with TickerProviderStateMixin{
               margin: const EdgeInsets.symmetric(horizontal: 4.0),
               child: CupertinoButton(
                 child: Text("보내기"),
-                onPressed: _exist
-                    ? () => _handleSubmitted(_textController.text)
-                    :null,
+                onPressed: _exist ? () => _handleSubmitted(_textController.text) :null,
+
               ),
             ),
           ],
@@ -86,18 +139,27 @@ class ChatRoomState extends State<ChatRoom> with TickerProviderStateMixin{
       ),
     );
   }
+
+  /// 불러온 메시지 값들을 받아서 이미 온 메시지들을 정리한다.
   void _aaa(){
-    Messages mas = Messages(text: "first message", animationController: AnimationController(duration : Duration(milliseconds: 700),vsync: this));
+    Messages mas = Messages(text: "first message", animationController: AnimationController(duration : Duration(milliseconds: 700),vsync: this), ismine: false,);
     setState(() {
       _message.insert(0, mas);
     });
     mas.animationController.forward();
+    Messages secondmas = Messages(text: "second message", animationController: AnimationController(duration : Duration(milliseconds: 700),vsync: this), ismine: true,);
+    setState(() {
+      _message.insert(0, secondmas);
+    });
+    secondmas.animationController.forward();
   }
+
 
   void _handleSubmitted(String text){
     _textController.clear();
     setState(() {
       _exist = false;
+      updatemessage(text);
     });
     Messages message = Messages(
       text: text,
@@ -105,6 +167,7 @@ class ChatRoomState extends State<ChatRoom> with TickerProviderStateMixin{
         duration: Duration(milliseconds: 700),
         vsync: this,
       ),
+      ismine: true,
     );
     setState(() {
       _message.insert(0,message);
@@ -125,8 +188,8 @@ class ChatRoomState extends State<ChatRoom> with TickerProviderStateMixin{
 class Messages extends StatelessWidget {
   final String text;
   final AnimationController animationController;
-
-  Messages({required this.text, required this.animationController});
+  final bool ismine;
+  Messages({required this.text, required this.animationController, required this.ismine});
 
   @override
   Widget build(BuildContext context) {
@@ -140,14 +203,14 @@ class Messages extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Container(
-              margin: const EdgeInsets.only(right: 16.0),
-              child: CircleAvatar(child: Text(_name[0])),
+              margin: ismine ? const EdgeInsets.only(right: 16.0,left: 200.0) : const EdgeInsets.only(right: 16.0),
+              child: ismine ? CircleAvatar(child: Text(_name[0])) : CircleAvatar(child: Text(_other[0])) ,
             ),
             Expanded(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  Text(_name),
+                  Text(ismine ? _name : _other),
                   Container(
                     margin: const EdgeInsets.only(top: 5.0),
                     child: Text(text),
--- flutter_messenger_app/lib/src/pages/ChatTab.dart
+++ flutter_messenger_app/lib/src/pages/ChatTab.dart
@@ -6,63 +6,63 @@ class ChatTab extends StatefulWidget {
   _ChatTabState createState() => _ChatTabState();
 }
 
-const List<String> friends = ["희원", "강민", "도윤"];
+const List<String> friends = ["윤도윤", "이강민"];
 
 class _ChatTabState extends State<ChatTab> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: SingleChildScrollView(
-      physics: BouncingScrollPhysics(),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          SafeArea(
-            child: Padding(
-              padding: EdgeInsets.only(left: 16, right: 16, top: 10),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[],
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 16, left: 16, right: 16),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: "Search...",
-                hintStyle: TextStyle(color: Colors.grey.shade600),
-                prefixIcon: Icon(
-                  Icons.search,
-                  color: Colors.grey.shade600,
-                  size: 20,
-                ),
-                filled: true,
-                fillColor: Colors.grey.shade100,
-                contentPadding: EdgeInsets.all(8),
-                enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20),
-                    borderSide: BorderSide(color: Colors.grey.shade100)),
-              ),
-            ),
-          ),
-          ListView.separated(
-              padding: EdgeInsets.only(top: 15),
-              scrollDirection: Axis.vertical,
-              shrinkWrap: true,
-              itemCount: friends.length,
-              itemBuilder: (BuildContext context, int index){
-                return GestureDetector(
-                  onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => ChatRoom(friends[index]))),
-                  child: Text(
-                    friends[index]
+          physics: BouncingScrollPhysics(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              SafeArea(
+                child: Padding(
+                  padding: EdgeInsets.only(left: 16, right: 16, top: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[],
                   ),
-                );
-              },
-              separatorBuilder: (BuildContext context, int index) => const Divider(),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 16, left: 16, right: 16),
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Search...",
+                    hintStyle: TextStyle(color: Colors.grey.shade600),
+                    prefixIcon: Icon(
+                      Icons.search,
+                      color: Colors.grey.shade600,
+                      size: 20,
+                    ),
+                    filled: true,
+                    fillColor: Colors.grey.shade100,
+                    contentPadding: EdgeInsets.all(8),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide(color: Colors.grey.shade100)),
+                  ),
+                ),
+              ),
+              ListView.separated(
+                padding: EdgeInsets.only(top: 15),
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                itemCount: friends.length,
+                itemBuilder: (BuildContext context, int index){
+                  return GestureDetector(
+                    onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => ChatRoom(friends[index]))),
+                    child: Text(
+                        friends[index]
+                    ),
+                  );
+                },
+                separatorBuilder: (BuildContext context, int index) => const Divider(),
+              ),
+            ],
           ),
-        ],
-      ),
-    ));
+        ));
   }
 }

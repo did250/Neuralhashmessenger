29339e5b75dfe07e8700ee516058efa976950b9d
did250 <gmldnjs2500@naver.com>
2022-08-23 20:22:18 +0900

    Fix

--- flutter_messenger_app/lib/src/pages/ChatTab.dart
+++ flutter_messenger_app/lib/src/pages/ChatTab.dart
@@ -6,12 +6,15 @@ import 'package:flutter_messenger_app/src/pages/ChatRoom.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:ntp/ntp.dart';
 
+List<Map<String,dynamic>> room = [];
+
+
 class ChatTab extends StatefulWidget {
   @override
-  _ChatTabState createState() => _ChatTabState();
+  ChatTabState createState() => ChatTabState();
 }
 
-class _ChatTabState extends State<ChatTab> {
+class ChatTabState extends State<ChatTab> {
 
   List<String> names = <String>[];
   List<int> numbers = <int>[];
@@ -86,13 +89,14 @@ class _ChatTabState extends State<ChatTab> {
                   if (snapshot.hasData) {
                     names.clear();
                     numbers.clear();
+                    room.clear();
+                    for (var item in (snapshot.data as DatabaseEvent).snapshot.value as List<Object?>) {
 
-                    for (var item in (snapshot.data as DatabaseEvent).snapshot
-                        .value as List<Object?>) {
                       Map<String, dynamic> map = Map<String, dynamic>.from(
                           item as Map<dynamic?, dynamic?>);
                       numbers.add(map["number"]);
                       names.add(map["with"]);
+                      room.add(map);
                     }
                     return ListView.separated(
                       padding: EdgeInsets.only(top: 15),

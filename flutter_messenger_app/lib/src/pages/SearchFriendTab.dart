6c811340566ba9d0d569ce882c4aad8d8d2129ea
doyoon0307 <ehdbs8609@naver.com>
2022-10-24 20:12:02 +0900

    UI

--- flutter_messenger_app/lib/src/pages/SearchFriendTab.dart
+++ flutter_messenger_app/lib/src/pages/SearchFriendTab.dart
@@ -22,32 +22,56 @@ class _SearchFriendTabState extends State<SearchFriendTab> {
 
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text("Search")),
-        body: Column(children: [
-          Padding(
-              padding: EdgeInsets.only(top: 16, left: 16, right: 16),
-              child: TextField(
-                controller: myController,
-                decoration: InputDecoration(
-                  hintText: "Search...",
-                  hintStyle: TextStyle(color: Colors.grey.shade600),
-                  prefixIcon: Icon(
-                    Icons.search,
-                    color: Colors.grey.shade600,
-                    size: 20,
+        appBar: AppBar(
+          title: Text("Search", style:TextStyle(fontSize: 16, color: Colors.black)),
+          iconTheme: IconThemeData(color: Colors.black,),
+          backgroundColor: Colors.white,
+          elevation: 0,
+        ),
+        body: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                  child: Container(
+                      height: 70,
+                      padding: EdgeInsets.only(top: 16, left: 16, right: 2),
+                      child:TextField(
+                        controller: myController,
+                        decoration: InputDecoration(
+                        hintText: "Search...",
+                        hintStyle: TextStyle(color: Colors.grey.shade600, fontWeight: FontWeight.bold),
+                        prefixIcon: Icon(
+                          Icons.search,
+                          color: Colors.grey.shade600,
+                          size: 20,
+                        ),
+                        filled: true,
+                        fillColor: Colors.grey.shade200,
+                        contentPadding: EdgeInsets.all(8),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(color: Colors.grey.shade200)),
+                        ),
+                      )
                   ),
-                  filled: true,
-                  fillColor: Colors.grey.shade100,
-                  contentPadding: EdgeInsets.all(8),
-                  enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                      borderSide: BorderSide(color: Colors.grey.shade100)),
-                ),
-              )),
-          TextButton(
-              onPressed: () =>
+                flex: 8,
+              ),
+              Expanded(
+                child: IconButton(
+                  iconSize: 30,
+                  icon: Icon(
+                    Icons.check,
+                    color: Colors.black,
+                  ),
+                  padding: EdgeInsets.all(5),
+                  constraints: BoxConstraints(),
+                  onPressed: () =>
                   {Navigator.pop(context, _searchFriend(myController.text))},
-              child: Text("button"))
-        ]));
+                ),
+                flex: 1,
+              ),
+            ]
+        )
+    );
   }
 }
